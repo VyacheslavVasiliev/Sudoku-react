38b145721f86782f1e2d65bf8147b829fd545a61
Vyacheslav Vasiliev <vyache.slav-vasiliev@yandex.ru>
2020-05-31 18:16:45 +0300

    change bundle config for adding on github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,76 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  output: {
-    filename: 'main-[hash:8].js',
-    publicPath: '/'
-  }, 
+module.exports = (env = {}) => {
+  const { production = false } = env;
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
+  return { 
+    output: {
+      filename: 'main-[hash:8].js',
+      publicPath: production ? './' : "/"
+    }, 
 
-  module: {
-    rules :[
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
 
-      // Настройка картинок
-      {
-        test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      },
+    module: {
+      rules :[
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
 
-      // Настройка картинок
-      {
-        test: /\.(png|jpg|jpeg|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]'
+        // Настройка картинок
+        {
+          test: /\.less$/,
+          use: ['style-loader', 'css-loader', 'less-loader']
+        },
+
+        // Настройка картинок
+        {
+          test: /\.(png|jpg|jpeg|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
             }
-          }
-        ]
-      },
+          ]
+        },
 
-      // Настройка шрифтов
-      {
-        test: /\.(woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]'
+        // Настройка шрифтов
+        {
+          test: /\.(woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
             }
-          }
-        ]
-      },
+          ]
+        },
+      ]
+    },
+
+    devServer: {
+      open: true,
+      contentBase: "./dist",
+      historyApiFallback: true
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        favicon: "./src/sudoku.ico",
+        base :  production ? './' : false,
+        template: "./src/index.html"
+      }),
+      new CleanWebpackPlugin()
     ]
-  },
-
-  devServer: {
-    open: true,
-    contentBase: "./dist",
-    historyApiFallback: true
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      favicon: "./src/sudoku.ico",
-      template: "./src/index.html"
-    }),
-    new CleanWebpackPlugin()
-  ]
+  }
 }
