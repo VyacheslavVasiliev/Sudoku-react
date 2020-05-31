b0fcf4e8430ae50167b9ac0a580393915f1d3269
Vyacheslav Vasiliev <vyache.slav-vasiliev@yandex.ru>
2020-05-31 18:01:34 +0300

    replaced browser router to hash router
    browser router incorrect work on github pages.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route
 } from "react-router-dom";
 
@@ -21,6 +21,6 @@ const App = () =>{
       </div>
     </main>
     )
-}
+} 
 
 export default App;
